2a0e0ee3e712e2c4b7b798caedc7081857128c34
Saeed Tabrizi <saeed@nowcando.com>
2020-02-17 23:35:31 +0330

    fix: assignee userId

--- src/bpms/bpmn/extensions/UserTaskExtension.ts
+++ src/bpms/bpmn/extensions/UserTaskExtension.ts
@@ -30,7 +30,7 @@ export const UserTaskExtension = (processInstance: BpmnProcessInstance | BpmnDef
                         activityType: api.type,
                         processDefinitionId: processInstance.DefinitionId,
                         processInstanceId: processInstance.Id,
-                        assignee: assignee,
+                        ['assignee.userId']: assignee.userId,
                         tenantId: bpms.Name,
                         completed: false,
                     });
